d007002bf144f936c8a017a03714370592dfba2f
还有一天就放假了 <13508120+itodouble@users.noreply.github.com>
2023-06-28 16:19:20 +0800

    Update generate_dockerfile.sh

--- generate_dockerfile.sh
+++ generate_dockerfile.sh
@@ -2,9 +2,7 @@ bash
 
 while read line;do  
     eval "$line"
-    echo "$line"
 done < config.propertis
-echo ${GITHUB_REF_NAME}
 
 echo "FROM alpine:3" > Dockerfile
 echo "LABEL email=\"itodouble@outlook.com\"" >> Dockerfile
@@ -13,12 +11,12 @@ echo "ARG FRP_VERSION=${GITHUB_REF_NAME}" >> Dockerfile
 echo "ARG URL=https://github.com/fatedier/frp/releases/download/v\${FRP_VERSION}/frp_\${FRP_VERSION}_linux_amd64.tar.gz" >> Dockerfile
 echo "" >> Dockerfile
 echo "## /usr/bin/{frps, frpc} -c xxx.ini" >> Dockerfile
-echo "RUN mkdir -p /frp \\" >> Dockerfile
+echo "RUN mkdir -p /frp /config \\" >> Dockerfile
 echo "    && cd /frp\\" >> Dockerfile
 echo "    && wget -qO- \${URL} | tar xz \\" >> Dockerfile
 echo "    && mv frp_*/frpc /usr/bin/ \\" >> Dockerfile
 echo "    && mv frp_*/frps /usr/bin/ \\" >> Dockerfile
-echo "    && mv frp_*/*.ini ./ \\" >> Dockerfile
+echo "    && mv frp_*/*.ini /config/ \\" >> Dockerfile
 echo "    && rm /frp -rf" >> Dockerfile
 echo "VOLUME /config" >> Dockerfile
 echo "ENV ARGS=frps CONFIG_FILE=frps" >> Dockerfile
