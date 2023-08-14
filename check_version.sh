bc460e5a18a8b23afbc1d7e5afecb2403ed12c17
还有一天就放假了 <13508120+itodouble@users.noreply.github.com>
2023-08-14 14:24:26 +0800

    Update check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -7,5 +7,29 @@ FRP_VERSION=${FRP_VERSION_FULL#v}
 
 if [ ${FRP_VERSION} != ${DOCKER_VERSION} ]
 then
-    ./generate_dockerfile.sh ${FRP_VERSION}
+    echo "构建版本："${FRP_VERSION}
+    
+    echo "FROM alpine:3" > Dockerfile
+    echo "LABEL email=\"itodouble@outlook.com\"" >> Dockerfile
+    echo "" >> Dockerfile
+    echo "RUN apk add --update" >> Dockerfile
+    echo "ARG FRP_VERSION=${GITHUB_REF_NAME}" >> Dockerfile
+    echo "ARG URL=https://github.com/fatedier/frp/releases/download/v\${FRP_VERSION}/frp_\${FRP_VERSION}_linux_amd64.tar.gz" >> Dockerfile
+    echo "" >> Dockerfile
+    echo "## /usr/bin/{frps, frpc} -c xxx.ini" >> Dockerfile
+    echo "RUN mkdir -p /frp /config \\" >> Dockerfile
+    echo "    && cd /frp\\" >> Dockerfile
+    echo "    && wget -qO- \${URL} | tar xz \\" >> Dockerfile
+    echo "    && mv frp_*/frpc /usr/bin/ \\" >> Dockerfile
+    echo "    && mv frp_*/frps /usr/bin/ \\" >> Dockerfile
+    echo "    && mv frp_*/*.ini /config/ \\" >> Dockerfile
+    echo "    && rm /frp -rf" >> Dockerfile
+    
+    echo "VOLUME /config" >> Dockerfile
+    
+    echo "ENV ARGS frps" >> Dockerfile
+    echo "ENV CONFIG_FILE frps.ini" >> Dockerfile
+    echo "ENV TZ Asia/Shanghai" >> Dockerfile
+    
+    echo "CMD /usr/bin/\${ARGS} -c /config/\${CONFIG_FILE}" >> Dockerfile
 fi
