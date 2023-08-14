c9ece0c083253c8f35608afc5bb89385d9995511
还有一天就放假了 <13508120+itodouble@users.noreply.github.com>
2023-08-14 13:59:52 +0800

    Update check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 bash
 DOCKER_IMAGE_INFO=$(curl -s https://hub.docker.com/v2/repositories/itodouble/frp/tags/)
 echo ${DOCKER_IMAGE_INFO}
-DOCKER_LATEST_INFO=$(jq '.results[1]' ${DOCKER_IMAGE_INFO})
+DOCKER_LATEST_INFO=$(echo ${DOCKER_IMAGE_INFO} | jq '.results[1]')
 echo ${DOCKER_LATEST_INFO}
 DOCKER_VERSION=$(echo "$DOCKER_LATEST_INFO" | jq -r '.images[0].name')
 echo $DOCKER_VERSION
