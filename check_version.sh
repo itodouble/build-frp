578ac4798cf31207366a40e64500be539d5f740b
还有一天就放假了 <13508120+itodouble@users.noreply.github.com>
2023-08-14 13:44:31 +0800

    Update check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -1,4 +1,4 @@
 bash
 
-DOCKER_VERSION=$(curl -s https://registry.hub.docker.com/v1/repositories/itodouble/frp/tags | jq -r '.tags[] | select(.name | test("v[0-9.]+")) | .name' | sort -V | tail -n 1)
+DOCKER_VERSION=$(curl -s https://registry.hub.docker.com/v1/repositories/itodouble/frp/tags | jq -r '.tags[] | select(.name | test(/v[0-9.]+/)) | .name' | sort -V | tail -n 1)
 echo $DOCKER_VERSION
