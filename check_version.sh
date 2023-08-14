2455f8f61fe60094877bec9b63c1a07b0e7a92d7
还有一天就放假了 <13508120+itodouble@users.noreply.github.com>
2023-08-14 14:07:15 +0800

    Update check_version.sh

--- check_version.sh
+++ check_version.sh
@@ -2,4 +2,10 @@ bash
 FRP_DOCKER_TAGS_URL=https://hub.docker.com/v2/repositories/itodouble/frp/tags
 DOCKER_VERSION=$(curl -s $FRP_DOCKER_TAGS_URL | jq -r '.results[1].name')
 
-echo $DOCKER_VERSION
+FRP_VERSION_FULL=`curl --silent "https://api.github.com/repos/fatedier/frp/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+FRP_VERSION=${FRP_VERSION_FULL#v}
+
+if [ ${FRP_VERSION} != ${DOCKER_VERSION} ]
+then
+	./generate_dockerfile.sh ${FRP_VERSION}
+fi
